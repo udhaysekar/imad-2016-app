ff0e87964df2da203944f396d50cd6809846f889
udhaysekar <udhaysekar6@gmail.com>
2016-09-18 21:29:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get("/article-one", function (req, res){
+    res.send("artivlr-one.requested and will be serve");
+});
+app.get("/article-two", function (req, res){
+    res.send("artivlr-two.requested and will be serve");
+});
+app.get("/article-three", function (req, res){
+    res.send("artivlr-three.requested and will be serve");
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
