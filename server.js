deed5ccf4a23c4a91d69d0cbe58ba5709a905836
udhaysekar <udhaysekar6@gmail.com>
2016-09-18 22:16:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get("/article-one", function (req, res){
-    res.send("artivlr-one.requested and will be serve 1");
+   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get("/article-two", function (req, res){
     res.send("artivlr-two.requested and will be serve 2");
