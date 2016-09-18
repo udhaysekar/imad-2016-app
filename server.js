51a4616cf756895affe99fc26ef41f995c1c42fa
udhaysekar <udhaysekar6@gmail.com>
2016-09-18 22:17:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get("/article-one", function (req, res){
+app.get("/articleone", function (req, res){
    res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get("/article-two", function (req, res){
